e3bc462e1c7ce58de7c89dc6e730437715d82a1e
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-10-05 20:35:58 +0200

    feat: add circle class
    Added Circle class with propery Area besides specified getArea() method.

---
+++ Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//   Defines the Circle type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Shapes
+{
+    public class Circle
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Radius
+        {
+            get => _radius;
+            set => _radius = value;
+        }
+
+        public double Area => GetArea();
+
+        /// <summary>
+        /// The get area.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public double GetArea()
+        {
+            return _radius * _radius * Math.PI;
+        }
+    }
+}
