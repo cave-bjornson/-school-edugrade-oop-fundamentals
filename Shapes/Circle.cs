cfc6ab4ec1c4e88b6e796f8efd467b8593f19243
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-10-06 00:36:57 +0200

    feat: add methods to circle class
    Add methods for circumference and volume.

--- Shapes/Circle.cs
+++ Shapes/Circle.cs
@@ -21,11 +21,31 @@
         /// Computes the area of the circle.
         /// </summary>
         /// <returns>
-        /// The <c>double</c> value of the circles area.
+        /// The <c>double</c> value of the area.
         /// </returns>
         public double GetArea()
         {
             return _radius * _radius * Math.PI;
+        }
+
+
+        /// <summary>
+        /// Computes the circumference of the circle.
+        /// </summary>
+        /// <returns>The <c>double</c> value of the circumference</returns>
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * _radius;
+        }
+
+
+        /// <summary>
+        /// Computes the volume of a globe with the same radius as this circle.
+        /// </summary>
+        /// <returns>The <c>double</c> value of the volume.</returns>
+        public double GetVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(_radius, 3.0);
         }
     }
 }
